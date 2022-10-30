2e6e1ddc68b517bd6f5dc8b198cc4febc9aae35e
BrunoPineda <bruno.pineda@urp.edu.pe>
2022-10-30 17:42:13 -0500

    login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using BackendSWGAF.Context;
+using BackendSWGAF.Helpers;
+using BackendSWGAF.Models.DTOs;
+using BackendSWGAF.Models.DTOs.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace BackendSWGAF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public AccountController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        //linea para mostrar los mensajes del sql procedimientos almacenados
+        public static string InfoMessageHandler(object mySender, SqlInfoMessageEventArgs myEvent)
+        {
+            return (myEvent.Message);
+        }
+        [HttpPost]
+        public IActionResult login([FromForm] LoginRequest request)
+        {
+            try
+            {
+                var log = SqlHelper.ExecuteNonQueryShowMessage(context, "sp_login", CommandType.StoredProcedure,
+                new SqlParameter("@email", request.email),
+                 new SqlParameter("@passsword", request.passsword)
+                 );
+                return Ok(new
+                {
+                    Res = true,
+                    StatusCode = 200,
+                    Message = log,
+                    Data = ""
+                }); ;
+            }
+            catch (Exception e)
+            {
+                return Ok(new
+                {
+                    Res = false,
+                    StatusCode = 500,
+                    Message = "Problema del servidor" + e,
+                    Data = ""
+                }); ;
+            }
+        }
+
+    }
+}
---
+++ Models/DTOs/LoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendSWGAF.Models.DTOs
+{
+    public class LoginRequest
+    {
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string passsword { get; set; }
+    }
+}
